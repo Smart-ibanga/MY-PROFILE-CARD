166ef8dfc7d88bdb99f97a29ccd2eb4f04a4e5d1
Smart-ibanga <ibangasmart@gmail.com>
2025-10-19 23:10:07 +0100

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Function to update the current UTC time
 function updateUTCTime() {
-    const currentTimeElement = document.querySelector('[data-testid="currentTimeUTC"]');
+    const currentTimeElement = document.querySelector('.current-time-utc');
     const now = new Date();
     const utcTime = now.toUTCString();
     currentTimeElement.textContent = `Current UTC Time: ${utcTime}`;
